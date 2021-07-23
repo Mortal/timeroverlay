8009349918d7d7665c500a626c38eab5cbb3d6c9
Mathias Rav <m@git.strova.dk>
2021-07-23 14:58:14 +0200

    WIP

--- view.js
+++ view.js
@@ -1,55 +1,12 @@
-var design = {
-    fontFamily: "sans-serif",
-    fontWeight: "700",
-    backgroundColor: "#111",
-    color: "#dde",
-    padding: "30px",
-    display: "inline-block"
-};
-var eventSpecs = [
-    {
-        mode: "countdown",
-        since: "21:05:00",
-        until: "22:00:00",
-        format: "m:ss",
-        text: "Next chug in {}"
-    },
-    {
-        mode: "stopwatch",
-        since: "22:00:00",
-        until: "22:05:00",
-        format: "m:ss.ff",
-        text: "{}"
-    },
-    {
-        mode: "countdown",
-        since: "22:05:00",
-        until: "22:30:00",
-        format: "m:ss",
-        text: "Next chug in {}"
-    },
-    {
-        mode: "stopwatch",
-        since: "22:30:00",
-        until: "22:35:00",
-        format: "m:ss.ff",
-        text: "{}"
-    },
-    {
-        mode: "countdown",
-        since: "22:35:00",
-        until: "23:00:00",
-        format: "m:ss",
-        text: "Next chug in {}"
-    },
-];
 function parseTime(t) {
-    var mo = /^(\d+):(\d+):(\d+)$/.exec(t);
-    return +mo[1] * 3600000 + +mo[2] * 60000 + +mo[3] * 1000;
+    var mo = /^(\d+):(\d+)(?::(\d+(?:\.\d*)?))?$/.exec(t);
+    return +mo[1] * 3600000 + +mo[2] * 60000 + +(mo[3] || "0") * 1000;
 }
 function parseEventSpec(eventSpec) {
     var sinceMillis = parseTime(eventSpec.since);
     var untilMillis = parseTime(eventSpec.until);
+    var paused = false;
+    var elapsed = 0;
     var pattern = /m+|s+|f+/g;
     var _a = eventSpec.text.split("{}"), before = _a[0], after = _a[1];
     var parts = [];
@@ -60,7 +17,7 @@ function parseEventSpec(eventSpec) {
             break;
         var j = pattern.lastIndex;
         var align = mo[0].length > 1 ? "00000".slice(0, mo[0].length) : "";
-        var extra = (i ? "" : before) + eventSpec.format.slice(i, j - mo[0].length);
+        var extra = eventSpec.format.slice(i, j - mo[0].length);
         var c = mo[0].charAt(0);
         if (c === "m")
             parts.push({ align: align, extra: extra, div: 60000, mod: 0 });
@@ -75,47 +32,130 @@ function parseEventSpec(eventSpec) {
         else
             parts.push({ align: align, extra: extra + "???", div: 1, mod: 0 });
     }
-    var computeText = function (t) { return parts.map(function (_a) {
+    var mapPart = function (t, _a) {
         var align = _a.align, extra = _a.extra, div = _a.div, mod = _a.mod;
-        return extra + (align + (((mod ? (t % (mod * div)) : t) / div) | 0)).substr(-align.length);
-    }).join("") + after; };
-    var computeTime = function (t) { return (eventSpec.mode === "countdown" ? (untilMillis - t) : (t - sinceMillis)); };
-    return { computeText: computeText, computeTime: computeTime, sinceMillis: sinceMillis, untilMillis: untilMillis };
+        if (mod) {
+            t = t % (mod * div);
+        }
+        t = (t / div) | 0;
+        return extra + (align + t).substr(-align.length);
+    };
+    var computeText = function (t) { return before + (t < 0 ? "-" : "") + parts.map(function (part) { return mapPart(Math.abs(t), part); }).join("") + after; };
+    var computeTime = function (t) { return (elapsed
+        + (paused ? 0 : (eventSpec.mode === "countdown" ? (untilMillis - t) : (t - sinceMillis)))); };
+    var keyHandlers = {};
+    if (eventSpec.mode === "stopwatch") {
+        keyHandlers["Space"] = function (t) {
+            elapsed = computeTime(t);
+            paused = !paused;
+            sinceMillis = t;
+        };
+        keyHandlers["KeyR"] = function (t) {
+            elapsed = 0;
+            paused = true;
+            sinceMillis = t;
+        };
+    }
+    return { computeText: computeText, computeTime: computeTime, sinceMillis: sinceMillis, untilMillis: untilMillis, keyHandlers: keyHandlers };
 }
-// console.log(parseEventSpec({mode: "countdown", since: "01:00:00", until: "02:00:00", format: "ss", text: "{}"}).computeText(1996000))
-function setup() {
-    var div = document.getElementById("timeroverlay");
+function setup(div, design, eventSpecs) {
     for (var _i = 0, _a = Object.keys(design); _i < _a.length; _i++) {
         var k = _a[_i];
         div.style[k] = design[k];
     }
-    var now = new Date();
-    var midnight = +new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    var nowDate = new Date();
+    var midnight = +new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate());
     var i = 0;
     var e = parseEventSpec(eventSpecs[i]);
+    var now = function () { return +new Date() - midnight; };
     function keydown(ev) {
+        if (e.keyHandlers[ev.code]) {
+            e.keyHandlers[ev.code](now(), ev);
+            return;
+        }
         if (ev.code === "ArrowLeft") {
             i -= 1;
         }
-        if (ev.code === "ArrowRight") {
+        else if (ev.code === "ArrowRight") {
             i += 1;
         }
-        console.log({ i: i });
+        else {
+            console.log({ code: ev.code });
+            return;
+        }
         i = Math.min(eventSpecs.length - 1, Math.max(0, i));
         e = parseEventSpec(eventSpecs[i]);
+        ev.preventDefault();
+        ev.stopPropagation();
     }
-    window.addEventListener("keydown", keydown, false);
     function update() {
-        var time = +new Date() - midnight;
+        var time = now();
         while (i + 1 < eventSpecs.length && e.untilMillis < time) {
             i += 1;
             e = parseEventSpec(eventSpecs[i]);
         }
         var t = e.computeTime(time);
-        // console.log({t})
         div.textContent = e.computeText(t);
-        window.requestAnimationFrame(update);
     }
-    window.requestAnimationFrame(update);
+    var updateCallback = function () {
+        update();
+        window.requestAnimationFrame(updateCallback);
+    };
+    window.requestAnimationFrame(updateCallback);
+    window.addEventListener("keydown", keydown, false);
 }
-window.addEventListener("load", setup, false);
+// console.log(parseEventSpec({mode: "countdown", since: "01:00:00", until: "02:00:00", format: "ss", text: "{}"}).computeText(1996000))
+window.addEventListener("load", function () {
+    var div = document.getElementById("timeroverlay");
+    var design = {
+        fontFamily: "sans-serif",
+        fontWeight: "700",
+        backgroundColor: "#000",
+        textShadow: "0 0 3px #111",
+        color: "#dde",
+        padding: "30px",
+        display: "inline-block",
+        width: "500px",
+        textAlign: "center",
+        height: "50px",
+        fontSize: "40px"
+    };
+    var eventSpecs = [
+        {
+            mode: "countdown",
+            since: "00:00:00",
+            until: "14:55:00",
+            format: "m:ss",
+            text: "Next chug in {}"
+        },
+        {
+            mode: "stopwatch",
+            since: "14:55:00",
+            until: "15:55:00",
+            format: "m:ss.ff",
+            text: "{}"
+        },
+        {
+            mode: "countdown",
+            since: "22:05:00",
+            until: "22:30:00",
+            format: "m:ss",
+            text: "Next chug in {}"
+        },
+        {
+            mode: "stopwatch",
+            since: "22:30:00",
+            until: "22:35:00",
+            format: "m:ss.ff",
+            text: "{}"
+        },
+        {
+            mode: "countdown",
+            since: "22:35:00",
+            until: "23:00:00",
+            format: "m:ss",
+            text: "Next chug in {}"
+        },
+    ];
+    setup(div, design, eventSpecs);
+}, false);
--- view.ts
+++ view.ts
@@ -1,11 +1,3 @@
-const design = {
-	fontFamily: "sans-serif",
-	fontWeight: "700",
-	backgroundColor: "#111",
-	color: "#dde",
-	padding: "30px",
-	display: "inline-block",
-};
 interface EventSpec {
 	mode: "countdown" | "stopwatch";
 	since: string;
@@ -13,115 +5,182 @@ interface EventSpec {
 	format: string;
 	text: string;
 }
+
 interface ParsedEvent {
 	computeText(currentTimeMillis: number): string;
 	computeTime(currentTimeMillis: number): number;
 	sinceMillis: number;
 	untilMillis: number;
+	keyHandlers: { [key: string]: (t: number, ev: KeyboardEvent) => void };
 }
-const eventSpecs: EventSpec[] = [
-	{
-		mode: "countdown",
-		since: "21:05:00",
-		until: "22:00:00",
-		format: "m:ss",
-		text: "Next chug in {}",
-	},
-	{
-		mode: "stopwatch",
-		since: "22:00:00",
-		until: "22:05:00",
-		format: "m:ss.ff",
-		text: "{}",
-	},
-	{
-		mode: "countdown",
-		since: "22:05:00",
-		until: "22:30:00",
-		format: "m:ss",
-		text: "Next chug in {}",
-	},
-	{
-		mode: "stopwatch",
-		since: "22:30:00",
-		until: "22:35:00",
-		format: "m:ss.ff",
-		text: "{}",
-	},
-	{
-		mode: "countdown",
-		since: "22:35:00",
-		until: "23:00:00",
-		format: "m:ss",
-		text: "Next chug in {}",
-	},
-];
-function parseTime(t: string): number {
-	const mo = /^(\d+):(\d+):(\d+)$/.exec(t);
-	return +mo[1] * 3600000 + +mo[2] * 60000 + +mo[3] * 1000;
-}
+
 interface Part {
 	align: string;
 	extra: string;
 	div: number;
 	mod: number;
 }
+
+function parseTime(t: string): number {
+	const mo = /^(\d+):(\d+)(?::(\d+(?:\.\d*)?))?$/.exec(t);
+	return +mo[1] * 3600000 + +mo[2] * 60000 + +(mo[3] || "0") * 1000;
+}
+
 function parseEventSpec(eventSpec: EventSpec): ParsedEvent {
-	const sinceMillis = parseTime(eventSpec.since);
+	let sinceMillis = parseTime(eventSpec.since);
 	const untilMillis = parseTime(eventSpec.until);
+	let paused = false;
+	let elapsed = 0;
 	const pattern = /m+|s+|f+/g;
 	const [before, after] = eventSpec.text.split("{}");
 	const parts: Part[] = [];
-	for (;;) {
+	for (; ;) {
 		const i = pattern.lastIndex;
 		const mo = pattern.exec(eventSpec.format);
 		if (mo == null) break;
 		const j = pattern.lastIndex;
 		const align: string = mo[0].length > 1 ? "00000".slice(0, mo[0].length) : "";
-		const extra = (i ? "" : before) + eventSpec.format.slice(i, j - mo[0].length);
+		const extra = eventSpec.format.slice(i, j - mo[0].length);
 		const c = mo[0].charAt(0);
-		if (c === "m") parts.push({align, extra, div: 60000, mod: 0});
-		else if (c === "s") parts.push({align, extra, div: 1000, mod: 60});
-		else if (mo[0] === "f") parts.push({align, extra, div: 100, mod: 10});
-		else if (mo[0] === "ff") parts.push({align, extra, div: 10, mod: 100});
-		else if (mo[0] === "fff") parts.push({align, extra, div: 1, mod: 1000});
-		else parts.push({align, extra: extra + "???", div: 1, mod: 0});
+		if (c === "m") parts.push({ align, extra, div: 60000, mod: 0 });
+		else if (c === "s") parts.push({ align, extra, div: 1000, mod: 60 });
+		else if (mo[0] === "f") parts.push({ align, extra, div: 100, mod: 10 });
+		else if (mo[0] === "ff") parts.push({ align, extra, div: 10, mod: 100 });
+		else if (mo[0] === "fff") parts.push({ align, extra, div: 1, mod: 1000 });
+		else parts.push({ align, extra: extra + "???", div: 1, mod: 0 });
 	}
-	const computeText = (t: number) => parts.map(({align, extra, div, mod}) => extra + (align + (((mod ? (t % (mod * div)) : t) / div)|0)).substr(-align.length)).join("") + after;
-	const computeTime = (t: number) => (eventSpec.mode === "countdown" ? (untilMillis - t) : (t - sinceMillis));
-	return {computeText, computeTime, sinceMillis, untilMillis};
+	const mapPart = (t: number, { align, extra, div, mod }: Part) => {
+		if (mod) {
+			t = t % (mod * div);
+		}
+		t = (t / div) | 0;
+		return extra + (align + t).substr(-align.length);
+	};
+	const computeText = (t: number) => before + (t < 0 ? "-" : "") + parts.map(
+		(part) => mapPart(Math.abs(t), part)
+	).join("") + after;
+	const computeTime = (t: number) => (
+		elapsed
+		+ (paused ? 0 : (eventSpec.mode === "countdown" ? (untilMillis - t) : (t - sinceMillis)))
+	);
+	const keyHandlers = {};
+	if (eventSpec.mode === "stopwatch") {
+		keyHandlers["Space"] = (t: number) => {
+			elapsed = computeTime(t);
+			paused = !paused;
+			sinceMillis = t;
+		};
+		keyHandlers["KeyR"] = (t: number) => {
+			elapsed = 0;
+			paused = true;
+			sinceMillis = t;
+		}
+	}
+	return { computeText, computeTime, sinceMillis, untilMillis, keyHandlers };
 }
-// console.log(parseEventSpec({mode: "countdown", since: "01:00:00", until: "02:00:00", format: "ss", text: "{}"}).computeText(1996000))
-function setup() {
-	const div = document.getElementById("timeroverlay");
+
+function setup(div: HTMLElement, design: { [k: string]: string }, eventSpecs: EventSpec[]) {
 	for (const k of Object.keys(design)) div.style[k] = design[k];
-	const now = new Date();
-	const midnight = +new Date(now.getFullYear(), now.getMonth(), now.getDate());
+	const nowDate = new Date();
+	const midnight = +new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate());
 	let i = 0;
 	let e = parseEventSpec(eventSpecs[i]);
+
+	const now = () => +new Date() - midnight;
+
 	function keydown(ev: KeyboardEvent) {
+		if (e.keyHandlers[ev.code]) {
+			e.keyHandlers[ev.code](now(), ev);
+			return;
+		}
 		if (ev.code === "ArrowLeft") {
 			i -= 1;
-		}
-		if (ev.code === "ArrowRight") {
+		} else if (ev.code === "ArrowRight") {
 			i += 1;
+		} else {
+			console.log({ code: ev.code })
+			return;
 		}
-		console.log({i})
 		i = Math.min(eventSpecs.length - 1, Math.max(0, i));
 		e = parseEventSpec(eventSpecs[i]);
+		ev.preventDefault();
+		ev.stopPropagation();
 	}
-	window.addEventListener("keydown", keydown, false);
+
 	function update() {
-		const time = +new Date() - midnight;
+		const time = now();
 		while (i + 1 < eventSpecs.length && e.untilMillis < time) {
 			i += 1;
 			e = parseEventSpec(eventSpecs[i]);
 		}
 		const t = e.computeTime(time);
-		// console.log({t})
 		div.textContent = e.computeText(t);
-		window.requestAnimationFrame(update);
 	}
-	window.requestAnimationFrame(update);
+
+	const updateCallback = () => {
+		update();
+		window.requestAnimationFrame(updateCallback);
+	}
+	window.requestAnimationFrame(updateCallback);
+	window.addEventListener("keydown", keydown, false);
 }
-window.addEventListener("load", setup, false)
+
+// console.log(parseEventSpec({mode: "countdown", since: "01:00:00", until: "02:00:00", format: "ss", text: "{}"}).computeText(1996000))
+
+window.addEventListener("load", () => {
+	const div = document.getElementById("timeroverlay");
+
+	const design = {
+		fontFamily: "sans-serif",
+		fontWeight: "700",
+		backgroundColor: "#000",
+		textShadow: "0 0 3px #111",
+		color: "#dde",
+		padding: "30px",
+		display: "inline-block",
+		width: "500px",
+		textAlign: "center",
+		height: "50px",
+		fontSize: "40px",
+	};
+
+	const eventSpecs: EventSpec[] = [
+		{
+			mode: "countdown",
+			since: "00:00:00",
+			until: "14:55:00",
+			format: "m:ss",
+			text: "Next chug in {}",
+		},
+		{
+			mode: "stopwatch",
+			since: "14:55:00",
+			until: "15:55:00",
+			format: "m:ss.ff",
+			text: "{}",
+		},
+		{
+			mode: "countdown",
+			since: "22:05:00",
+			until: "22:30:00",
+			format: "m:ss",
+			text: "Next chug in {}",
+		},
+		{
+			mode: "stopwatch",
+			since: "22:30:00",
+			until: "22:35:00",
+			format: "m:ss.ff",
+			text: "{}",
+		},
+		{
+			mode: "countdown",
+			since: "22:35:00",
+			until: "23:00:00",
+			format: "m:ss",
+			text: "Next chug in {}",
+		},
+	];
+
+	setup(div, design, eventSpecs);
+}, false);
